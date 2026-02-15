ddc5d80038942607c58c07bea5fe8eb1311fca3a
Utkarsh Gill <ugill@ee.iitr.ac.in>
2026-02-15 22:58:16 +0530

    bump ent coef

--- experiments/exp050_rich_ppo/train.py
+++ experiments/exp050_rich_ppo/train.py
@@ -26,7 +26,7 @@ DELTA_SCALE, MAX_DELTA = 0.25, 0.5
 PI_LR, VF_LR     = 3e-4, 3e-4
 GAMMA, LAMDA      = 0.95, 0.9
 K_EPOCHS, EPS_CLIP = 4, 0.2
-VF_COEF, ENT_COEF = 1.0, 0.003
+VF_COEF, ENT_COEF = 1.0, 0.01
 MINI_BS           = int(os.getenv('MINI_BS', '100000'))
 CRITIC_WARMUP     = 4
 
